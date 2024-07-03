bd116a3c3d9943675220533acecb8f1897165575
Vanshaj Saxena <vs110405@outlook.com>
2024-07-03 16:12:50 +0530

    Initialised new project with SwiftUI and Firebase

---
+++ LMS_Admin_Librarian/LMS_Admin_LibrarianApp.swift
@@ -0,0 +1,31 @@
+//
+//  LMS_Admin_LibrarianApp.swift
+//  LMS_Admin_Librarian
+//
+//  Created by Aida Sharon Bruce on 03/07/24.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseDatabase
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct LMS_Admin_LibrarianApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
