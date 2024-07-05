5b1a257a040c4b6918ce96101c992509fa3a1d00
Missgarg710 <mahak.21scse1010213@galgotiasuniversity.edu.in>
2024-07-05 10:58:47 +0530

    some improvements done

--- LMS_Admin_Librarian/LoginView.swift
+++ LMS_Admin_Librarian/LoginView.swift
@@ -8,78 +8,96 @@ struct LoginView: View {
     @State private var isRememberMe: Bool = false
     @State private var isShowingForgotPassword: Bool = false
     @State private var loginError: String?
-    @State private var isAdmin: Bool = false
-    @State private var isLibrarian: Bool = false
+    @State private var navigationPath = NavigationPath()
 
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color("BackgroundColor")
-                    .edgesIgnoringSafeArea(.all)
-                VStack(alignment: .center, spacing: 30) {
-                    Text("Welcome Back 👋")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding(30)
-                    
-                    TextField("Email", text: $email)
-                        .padding()
-                        .background(Color(.white))
-                        .cornerRadius(12)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                    
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(.white))
-                        .cornerRadius(12)
-                    
-                    if let loginError = loginError {
-                        Text(loginError)
-                            .foregroundColor(.red)
-                            .padding()
-                    }
-                    
+        NavigationStack(path: $navigationPath) {
+            GeometryReader { geometry in
+                ZStack {
+                    Color("BackgroundColor")
+                        .edgesIgnoringSafeArea(.all)
                     HStack {
-                        Button(action: {
-                            isRememberMe.toggle()
-                        }) {
-                            HStack {
-                                Image(systemName: isRememberMe ? "checkmark.square" : "square")
-                                Text("Remember me!")
+                        VStack(alignment: .center, spacing: 30) {
+                            Text("Welcome Back 👋")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(30)
+                            
+                            TextField("Email", text: $email)
+                                .padding()
+                                .background(Color(.white))
+                                .cornerRadius(12)
+                                .keyboardType(.emailAddress)
+                                .autocapitalization(.none)
+                            
+                            SecureField("Password", text: $password)
+                                .padding()
+                                .background(Color(.white))
+                                .cornerRadius(12)
+                            
+                            if let loginError = loginError {
+                                Text(loginError)
+                                    .foregroundColor(.red)
+                                    .padding()
                             }
-                        }
-                        Spacer()
-                        Button(action: {
-                            isShowingForgotPassword.toggle()
-                        }) {
-                            Text("Forgot Password?")
-                                .foregroundColor(.blue)
-                        }
-                    }
-                    .padding(.leading)
-                    .padding(.trailing)
-                    
-                    Button(action: login) {
-                        Text("Login")
-                            .foregroundColor(.white)
+                            
+                            HStack {
+                                Button(action: {
+                                    isRememberMe.toggle()
+                                }) {
+                                    HStack {
+                                        Image(systemName: isRememberMe ? "checkmark.square" : "square")
+                                        Text("Remember me!")
+                                    }
+                                }
+                                Spacer()
+                                Button(action: {
+                                    isShowingForgotPassword.toggle()
+                                }) {
+                                    Text("Forgot Password?")
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding(.leading)
+                            .padding(.trailing)
+                            
+                            Button(action: login) {
+                                Text("Login")
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(width: geometry.size.width * 0.4)
+                                    .background(Color("ThemeOrange"))
+                                    .cornerRadius(12)
+                                    .padding(.top, 50)
+                            }
                             .padding()
-                            .frame(width: 317)
-                            .background(Color("ThemeOrange"))
-                            .cornerRadius(12)
-                            .padding(.top, 50)
+                        }
+                        .frame(width: geometry.size.width * 0.4)
+                        
+                        Spacer()
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color("ThemeOrange"))
+                            .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.9)
+                            .overlay(
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.9)
+                            )
+                            .cornerRadius(20)
+                        
+                        Spacer()
                     }
                     .padding()
-                    
-                    NavigationLink(destination: AdminView(), isActive: $isAdmin) {
-                        EmptyView()
-                    }
-                    
-                    NavigationLink(destination: LibrarianView(), isActive: $isLibrarian) {
-                        EmptyView()
+                }
+                .navigationDestination(for: String.self) { destination in
+                    if destination == "AdminView" {
+                        AdminView()
+                    } else if destination == "LibrarianView" {
+                        LibrarianView()
                     }
                 }
-                .padding()
             }
         }
     }
@@ -93,7 +111,9 @@ struct LoginView: View {
                    let storedEmail = adminData["email"] as? String,
                    let storedPassword = adminData["password"] as? String {
                     if email == storedEmail && password == storedPassword {
-                        isAdmin = true
+                        DispatchQueue.main.async {
+                            navigateToView(view: "AdminView")
+                        }
                         return
                     }
                 }
@@ -112,9 +132,15 @@ struct LoginView: View {
                     return
                 }
 
-                isLibrarian = true
+                DispatchQueue.main.async {
+                    navigateToView(view: "LibrarianView")
+                }
             }
         }
+    }
+    
+    func navigateToView(view: String) {
+        navigationPath.append(view)
     }
 }
 
