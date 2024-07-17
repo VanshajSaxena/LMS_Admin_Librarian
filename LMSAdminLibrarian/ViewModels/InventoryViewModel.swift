6e04ce6cd92c43e47e744cef2a0dd3868d0d5d90
ShubhamGuptadln <sagarguptadln00@gmail.com>
2024-07-17 17:07:32 +0530

    added search feature to inventory view

--- LMSAdminLibrarian/ViewModels/InventoryViewModel.swift
+++ LMSAdminLibrarian/ViewModels/InventoryViewModel.swift
@@ -10,25 +10,44 @@ import SwiftUI
 import Firebase
 import FirebaseFirestore
 
+
 @MainActor
 final class InventoryViewModel: ObservableObject {
     @Published var books: [BookMetaData] = []
-    @Published var searchQuery: String = ""
+    @Published var allBooks: [BookMetaData] = []
+    @Published var searchQuery: String = "" {
+        didSet {
+            searchBooks(query: searchQuery)
+        }
+    }
     
     private let bookMetaDataService = BookMetaDataService()
     
-    
     func updateInventoryTableView(isbnList: [String]) async {
         do {
-            
             print("Fetched ISBN List: \(isbnList)")
             let completeData = await self.bookMetaDataService.getCompleteBookMetadata(isbnList: isbnList)
             print("Fetched Complete Data Count: \(completeData.count)")
             await MainActor.run {
                 self.books.removeAll()
                 self.books = completeData
-                print("Updated UI \(self.books.count) books")
+                self.allBooks = completeData
+                print("Updated UI with \(self.books.count) books")
             }
+        }
+    }
+    
+    func searchBooks(query: String) {
+        if query.isEmpty {
+            self.books = self.allBooks
+            return
+        }
+        
+        let lowercasedQuery = query.lowercased()
+        self.books = self.allBooks.filter { book in
+            book.title.lowercased().contains(lowercasedQuery) ||
+            book.authors.lowercased().contains(lowercasedQuery) ||
+            book.genre.lowercased().contains(lowercasedQuery)
         }
     }
 }
